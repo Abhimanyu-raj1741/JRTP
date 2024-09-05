3771025e2a495893a43176f4d79740a6e0659e5a
Abhimanyu <abhimanyu.raj1741@gmail.com>
2024-09-06 01:56:42 +0530

    update

---
+++ 11_USER_API/src/main/java/in/ashokit/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package in.ashokit.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UserEntity {
+	
+	private Integer userid;
+	
+	private String name;
+	
+	private String email;
+	
+	private String pwd;
+	
+	private String gender;
+	
+	private Long phno;
+	
+	private Long ssn;
+	
+
+}
