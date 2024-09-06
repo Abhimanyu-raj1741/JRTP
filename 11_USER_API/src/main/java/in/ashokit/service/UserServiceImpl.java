bc47c963b48ded6ba9a2780c0d58b0e6fc5a5b6c
Abhimanyu <abhimanyu.raj1741@gmail.com>
2024-09-07 02:05:10 +0530

    update

---
+++ 11_USER_API/src/main/java/in/ashokit/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package in.ashokit.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.ashokit.repo.UserInfoRepository;
+import in.ashokit.request.LoginRequest;
+import in.ashokit.request.PwdChangeRequest;
+import in.ashokit.request.SignUpRequest;
+import in.ashokit.response.LoginResponse;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserInfoRepository userrepo;
+	
+	
+	@Override
+	public boolean saveUser(SignUpRequest request) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public LoginResponse usserLogin(LoginRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LoginResponse updatePwd(PwdChangeRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean recoverPwd(String email) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
